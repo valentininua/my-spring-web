4cacbbc180486aebf993c4847f95ec6e37714bb7
Valentin Badiul S <380961726999@ya.ru>
2025-06-12 22:28:09 +0300

    info

--- readme.md
+++ readme.md
@@ -10,3 +10,15 @@ mvn clean install
 mvn spring-boot:run
 
 http://localhost:8080
+
+
+
+
+
+
+------
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-web</artifactId>
+</dependency>
+
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,19 +1,19 @@
-// package com.example.demo;
+package com.example.demo;
 
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class HelloController {
+@RestController
+public class HelloController {
 
-//     @GetMapping("/")
-//     public String home() {
-//         return "Сервер работает!";
-//     }
+    @GetMapping("/")
+    public String home() {
+        return "Сервер работает!";
+    }
 
-//     @GetMapping("/api/hello")
-//     public String hello() {
-//         return "Привет, Spring Boot!";
-//     }
-// }
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Привет, Spring Boot!";
+    }
+}
 

54ab19586c99106a7b6133f07f3f3b1c19fd06e8
Valentin Badiul S <380961726999@ya.ru>
2025-06-12 22:26:00 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+unzip demo.zip
+cd demo
+
+
+./mvnw spring-boot:run
+
+mvn clean install
+
+
+mvn spring-boot:run
+
+http://localhost:8080
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+// package com.example.demo;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// public class HelloController {
+
+//     @GetMapping("/")
+//     public String home() {
+//         return "Сервер работает!";
+//     }
+
+//     @GetMapping("/api/hello")
+//     public String hello() {
+//         return "Привет, Spring Boot!";
+//     }
+// }
+
